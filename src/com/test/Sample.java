9d1432677d7f964b1974fc0e7d7e99a7b424f38a
dockergit248 <141702767+dockergit248@users.noreply.github.com>
2024-05-16 00:05:29 +0530

    Update Sample.java

--- src/com/test/Sample.java
+++ src/com/test/Sample.java
@@ -3,6 +3,6 @@ Class Sample{
 public static void main(String args[]){
 System.out.println("Hello World");
 
-System.out.println("Testing to trigger build in Jenkins");
+System.out.println("Testing to trigger build in Jenkins 2");
 }
 }
